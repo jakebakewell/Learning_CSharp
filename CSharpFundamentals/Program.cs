ae73b13e525acbef4cb6874045f93fa7af9fcca5
unknown <bakewelljake@gmail.com>
2021-06-24 16:55:06 -0500

    More C# Basics

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Loop from 1 to 255
+            Console.WriteLine("********** Loop 1-255");
+            for (int i = 1; i < 256; i++) {
+                Console.WriteLine(i);
+            }
+            //Loop through 1-100 and print all values that are divisible by 3 or 5 but not both
+            Console.WriteLine("Loop through 1-100 printing multiples");
+            for (int i = 1; i < 101; i++) {
+                if(!(i % 5 == 0 && i % 3 == 0)) {
+                    if(i % 5 == 0 || i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            // Same as previous 1-100 loop but print fizz and buzz instead
+            Console.WriteLine("Loop through 1-100 printing fizz and buzz for multiples");
+            for (int i = 1; i < 101; i++) {
+                if (i % 15 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+            }
+            //Same loops as above but using while instead of for
+            Console.WriteLine("********** While Loop 1-255");
+            int j = 1;
+            while (j < 256) {
+                Console.WriteLine(j);
+                j++;
+            }
+            Console.WriteLine("Loop through 1-100 printing multiples");
+            j = 1;
+            while (j < 101) {
+                if(!(j % 5 == 0 && j % 3 == 0)) {
+                    if(j % 5 == 0 || j % 3 == 0)
+                    {
+                        Console.WriteLine(j);
+                    }
+                }
+                j++;
+            }
+                Console.WriteLine("Loop through 1-100 printing fizz and buzz for multiples");
+                j = 1;
+            while (j < 101) {
+                if (j % 15 == 0) {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (j % 3 == 0) {
+                    Console.WriteLine("Fizz");
+                }
+                else if (j % 5 == 0) {
+                    Console.WriteLine("Buzz");
+                }
+                j++;
+            }
+        }
+    }
+}
---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] numArray = {0,1,2,3,4,5,6,7,8,9};
+            string[] nameArray = new string[4];
+            nameArray[0] = "Tim";
+            nameArray[1] = "Martin";
+            nameArray[2] = "Nikki";
+            nameArray[3] = "Sara";
+            bool[] boolArray = {true, false, true, false, true, false, true, false, true, false};
+            List<string> flavors = new List<string>();
+            flavors.Add("chocolate");
+            flavors.Add("vanilla");
+            flavors.Add("strawberry");
+            flavors.Add("rocky road");
+            flavors.Add("cookie dough");
+            Console.WriteLine(flavors.Count);
+            Console.WriteLine(flavors[2]);
+            flavors.Remove("strawberry");
+            Console.WriteLine(flavors.Count);
+            Dictionary<string,string> favFlavors = new Dictionary<string,string>();
+            favFlavors.Add("Tim", "vanilla");
+            favFlavors.Add("Nikki", "chocolate");
+            favFlavors.Add("Martin", "cookie dough");
+            favFlavors.Add("Sara", "rocky road");
+            foreach (KeyValuePair<string,string> entry in favFlavors) {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+    }
+}
